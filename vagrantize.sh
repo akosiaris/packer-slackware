ce8ad3dd8e6e49a63987baedaeee250441190859
Alexandros Kosiaris <akosiaris@gmail.com>
2018-11-23 09:43:21 +0200

    Create vagrant home and set ownership
    useradd does not create /home/vagrant and does not set the owner, do
    that ourselves

--- vagrantize.sh
+++ vagrantize.sh
@@ -12,6 +12,8 @@ umount /mnt
 # Set up the vagrant user and give him full access
 groupadd vagrant
 useradd -d /home/vagrant -s /bin/bash -g vagrant vagrant
+mkdir /home/vagrant
+chown vagrant:vagrant /home/vagrant
 echo "vagrant:vagrant" | chpasswd
 echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su -c "umask 077 && mkdir /home/vagrant/.ssh && curl -O /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub" vagrant
